76b2cb837d68ef711a21d2f406f4bc8c6b524e4a
Lucas Izumi <lucasizumi@gmail.com>
2020-11-03 01:21:32 -0300

    Configuracao gunicorn

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import mtginfo
-from gevent.pywsgi import WSGIServer
+# from gevent.pywsgi import WSGIServer
 
 
 app = Flask(__name__)
@@ -36,5 +36,6 @@ def upload_file():
 
 # app.run(port=5400, debug=True)
 # app.run(host='0.0.0.0', port=5000)
-http_server = WSGIServer(('', 5000), app)
-http_server.serve_forever()
+# http_server = WSGIServer(('', 5000), app)
+# http_server.serve_forever()
+app.run()
