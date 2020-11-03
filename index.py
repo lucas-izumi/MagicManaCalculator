ac11b6115fa9ad1975da20a3f09e3e9d0bf4bcb8
Lucas Izumi <lucasizumi@gmail.com>
2020-11-03 01:26:17 -0300

    Protegendo contra multiplas instancias

--- index.py
+++ index.py
@@ -38,4 +38,5 @@ def upload_file():
 # app.run(host='0.0.0.0', port=5000)
 # http_server = WSGIServer(('', 5000), app)
 # http_server.serve_forever()
-app.run()
+if __name__ == '__main__':
+    app.run()
